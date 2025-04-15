226bd6578480f3853a63658634a06551f921e27e
SamuelKaleiba <axelgummesson90@gmail.com>
2025-04-15 15:40:19 +0200

    Update db_connection.py

--- scripts/db_connection.py
+++ scripts/db_connection.py
@@ -6,7 +6,7 @@ def get_connection():
     rds_host = os.environ.get('RDS_HOST', 'database-2.ch0eo28sely1.eu-central-1.rds.amazonaws.com')
     rds_user = os.environ.get('RDS_USER', 'admin')
     rds_password = os.environ.get('RDS_PASSWORD', 'grupp1ai')
-    rds_db_name = os.environ.get('RDS_DB_NAME', 'master')
+    rds_db_name = os.environ.get('RDS_DB_NAME', 'SmartFarming')
     rds_port = os.environ.get('RDS_PORT', '1433')  
 
     conn_str = (
